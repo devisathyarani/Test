667e073a31af9fdd34746aa6cb72f2e1fdb11136
devisathyarani <sathyadevi1290@gmail.com>
2018-10-15 11:02:15 +0530

    created base class

---
+++ src/org/tcs/Student.java
@@ -0,0 +1,15 @@
+package org.tcs;
+
+public class Student {
+	
+	public static void method()
+	{
+		System.out.println("My base class");
+	}
+	
+public static void main(String[] args) {
+	method();
+}
+}
+
+
